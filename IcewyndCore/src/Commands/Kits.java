b164e4fe247eb9b1c0cd42abac3dc56dc125c1d1
Mag <Mag@DESKTOP-GCOO917>
2018-02-03 23:20:05 -0500

    Added /kit

--- IcewyndCore/src/Commands/Kits.java
+++ IcewyndCore/src/Commands/Kits.java
@@ -3,16 +3,13 @@ package Commands;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Bukkit;
-import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
@@ -26,19 +23,350 @@ Main plugin = Main.getPlugin(Main.class);
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("kit")) {
 			Player player = (Player) sender;
-			if (sender instanceof Player) {
+			if (args.length == 0) {
 				openKitInventory(player);
-			}else if (args[0].equalsIgnoreCase("Icewynd")) {
-				ItemStack Item1 = new ItemStack(Material.PISTON_BASE, 1);
-				ItemMeta Item1Meta = Item1.getItemMeta();
-				List<String> lore1 = new ArrayList<String>();
-				Item1Meta.setDisplayName("§b§l§nIcewynd Kit Crate");
-				lore1.add("§ftest");
-				Item1Meta.setLore(lore1);
-				Item1Meta.addEnchant(Enchantment.DIG_SPEED, 1, true);
-				Item1Meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-				Item1.setItemMeta(Item1Meta);
-				player.getInventory().addItem(Item1);
+			}else if (args.length == 1) {
+				if (args[0].equalsIgnoreCase("fighter")) {
+					//Fighter Helmet
+					ItemStack Item1 = new ItemStack(Material.DIAMOND_HELMET, 1);
+					ItemMeta Item1Meta = Item1.getItemMeta();
+					Item1Meta.setDisplayName("§2Fighter Helmet");
+					Item1Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 2, true);
+					Item1Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item1.setItemMeta(Item1Meta);
+					
+					//Fighter Chestplate
+					ItemStack Item2 = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+					ItemMeta Item2Meta = Item2.getItemMeta();
+					Item2Meta.setDisplayName("§2Fighter Chestplate");
+					Item2Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 2, true);
+					Item2Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item2.setItemMeta(Item2Meta);
+					
+					//Fighter Leggings
+					ItemStack Item3 = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+					ItemMeta Item3Meta = Item3.getItemMeta();
+					Item3Meta.setDisplayName("§2Fighter Leggings");
+					Item3Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 2, true);
+					Item3Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item3.setItemMeta(Item3Meta);
+					
+					//Fighter Boots
+					ItemStack Item4 = new ItemStack(Material.DIAMOND_BOOTS, 1);
+					ItemMeta Item4Meta = Item4.getItemMeta();
+					Item4Meta.setDisplayName("§2Fighter Boots");
+					Item4Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 2, true);
+					Item4Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item4.setItemMeta(Item4Meta);
+					
+					//Fighter Sword
+					ItemStack Item5 = new ItemStack(Material.DIAMOND_SWORD, 1);
+					ItemMeta Item5Meta = Item5.getItemMeta();
+					Item5Meta.setDisplayName("§2Fighter Sword");
+					Item5Meta.addEnchant(Enchantment.DAMAGE_ALL, 2, true);
+					Item5.setItemMeta(Item5Meta);
+					
+					//Fighter Bow
+					ItemStack Item6 = new ItemStack(Material.BOW, 1);
+					ItemMeta Item6Meta = Item6.getItemMeta();
+					Item6Meta.setDisplayName("§2Fighter Bow");
+					Item6Meta.addEnchant(Enchantment.ARROW_DAMAGE, 2, true);
+					Item6.setItemMeta(Item6Meta);
+					
+					//Arrows
+					ItemStack Item7 = new ItemStack(Material.ARROW, 128);
+					
+					player.getInventory().addItem(Item1);
+					player.getInventory().addItem(Item2);
+					player.getInventory().addItem(Item3);
+					player.getInventory().addItem(Item4);
+					player.getInventory().addItem(Item5);
+					player.getInventory().addItem(Item6);
+					player.getInventory().addItem(Item7);
+				}else if (args[0].equalsIgnoreCase("warlord")) {
+					//Warlord Helmet
+					ItemStack Item1 = new ItemStack(Material.DIAMOND_HELMET, 1);
+					ItemMeta Item1Meta = Item1.getItemMeta();
+					Item1Meta.setDisplayName("§dWarlord Helmet");
+					Item1Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 3, true);
+					Item1Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item1.setItemMeta(Item1Meta);
+					
+					//Warlord Chestplate
+					ItemStack Item2 = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+					ItemMeta Item2Meta = Item2.getItemMeta();
+					Item2Meta.setDisplayName("§dWarlord Chestplate");
+					Item2Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 3, true);
+					Item2Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item2.setItemMeta(Item2Meta);
+					
+					//Warlord Leggings
+					ItemStack Item3 = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+					ItemMeta Item3Meta = Item3.getItemMeta();
+					Item3Meta.setDisplayName("§dWarlord Leggings");
+					Item3Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 3, true);
+					Item3Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item3.setItemMeta(Item3Meta);
+					
+					//Warlord Boots
+					ItemStack Item4 = new ItemStack(Material.DIAMOND_BOOTS, 1);
+					ItemMeta Item4Meta = Item4.getItemMeta();
+					Item4Meta.setDisplayName("§dWarlord Boots");
+					Item4Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 3, true);
+					Item4Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item4.setItemMeta(Item4Meta);
+					
+					//Warlord Sword
+					ItemStack Item5 = new ItemStack(Material.DIAMOND_SWORD, 1);
+					ItemMeta Item5Meta = Item5.getItemMeta();
+					Item5Meta.setDisplayName("§dWarlord Sword");
+					Item5Meta.addEnchant(Enchantment.DAMAGE_ALL, 3, true);
+					Item5.setItemMeta(Item5Meta);
+					
+					//Warlord Bow
+					ItemStack Item6 = new ItemStack(Material.BOW, 1);
+					ItemMeta Item6Meta = Item6.getItemMeta();
+					Item6Meta.setDisplayName("§dWarlord Bow");
+					Item6Meta.addEnchant(Enchantment.ARROW_DAMAGE, 3, true);
+					Item6.setItemMeta(Item6Meta);
+					
+					//Arrows
+					ItemStack Item7 = new ItemStack(Material.ARROW, 128);
+					
+					player.getInventory().addItem(Item1);
+					player.getInventory().addItem(Item2);
+					player.getInventory().addItem(Item3);
+					player.getInventory().addItem(Item4);
+					player.getInventory().addItem(Item5);
+					player.getInventory().addItem(Item6);
+					player.getInventory().addItem(Item7);
+				}else if (args[0].equalsIgnoreCase("emporer")) {
+					//Emporer Helmet
+					ItemStack Item1 = new ItemStack(Material.DIAMOND_HELMET, 1);
+					ItemMeta Item1Meta = Item1.getItemMeta();
+					Item1Meta.setDisplayName("§cEmporer Helmet");
+					Item1Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item1Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item1.setItemMeta(Item1Meta);
+					
+					//Emporer Chestplate
+					ItemStack Item2 = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+					ItemMeta Item2Meta = Item2.getItemMeta();
+					Item2Meta.setDisplayName("§cEmporer Chestplate");
+					Item2Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item2Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item2.setItemMeta(Item2Meta);
+					
+					//Emporer Leggings
+					ItemStack Item3 = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+					ItemMeta Item3Meta = Item3.getItemMeta();
+					Item3Meta.setDisplayName("§cEmporer Leggings");
+					Item3Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item3Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item3.setItemMeta(Item3Meta);
+					
+					//Emporer Boots
+					ItemStack Item4 = new ItemStack(Material.DIAMOND_BOOTS, 1);
+					ItemMeta Item4Meta = Item4.getItemMeta();
+					Item4Meta.setDisplayName("§cEmporer Boots");
+					Item4Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item4Meta.addEnchant(Enchantment.DURABILITY, 1, true);
+					Item4.setItemMeta(Item4Meta);
+					
+					//Emporer Sword
+					ItemStack Item5 = new ItemStack(Material.DIAMOND_SWORD, 1);
+					ItemMeta Item5Meta = Item5.getItemMeta();
+					Item5Meta.setDisplayName("§cEmporer Sword");
+					Item5Meta.addEnchant(Enchantment.DAMAGE_ALL, 4, true);
+					Item5.setItemMeta(Item5Meta);
+					
+					//Emporer Bow
+					ItemStack Item6 = new ItemStack(Material.BOW, 1);
+					ItemMeta Item6Meta = Item6.getItemMeta();
+					Item6Meta.setDisplayName("§cEmporer Bow");
+					Item6Meta.addEnchant(Enchantment.ARROW_DAMAGE, 3, true);
+					Item6.setItemMeta(Item6Meta);
+					
+					//Arrows
+					ItemStack Item7 = new ItemStack(Material.ARROW, 128);
+					
+					//Elixir of Fury
+					ItemStack Item8 = new ItemStack(Material.POTION, 1, (byte) 8266);
+					ItemMeta Item8Meta = Item6.getItemMeta();
+					List<String> lore8 = new ArrayList<String>();
+					Item8Meta.setDisplayName("§4§l§nElixir of Fury");
+					Item8Meta.addEnchant(Enchantment.DIG_SPEED, 5, true);
+					lore8.add("§eSpeed II (1:30)");
+					lore8.add("§eStrength II (1:30)");
+					lore8.add("§eFire Resistence (8:00)");
+					lore8.add("§1");
+					lore8.add("§cPrice: §f5 Coins");
+					Item8Meta.setLore(lore8);
+					Item8.setItemMeta(Item8Meta);
+					
+					player.getInventory().addItem(Item1);
+					player.getInventory().addItem(Item2);
+					player.getInventory().addItem(Item3);
+					player.getInventory().addItem(Item4);
+					player.getInventory().addItem(Item5);
+					player.getInventory().addItem(Item6);
+					player.getInventory().addItem(Item7);
+					player.getInventory().addItem(Item8);
+				}else if (args[0].equalsIgnoreCase("god")) {
+					//God Helmet
+					ItemStack Item1 = new ItemStack(Material.DIAMOND_HELMET, 1);
+					ItemMeta Item1Meta = Item1.getItemMeta();
+					Item1Meta.setDisplayName("§9§lGod Helmet");
+					Item1Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item1Meta.addEnchant(Enchantment.DURABILITY, 2, true);
+					Item1.setItemMeta(Item1Meta);
+					
+					//God Chestplate
+					ItemStack Item2 = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+					ItemMeta Item2Meta = Item2.getItemMeta();
+					Item2Meta.setDisplayName("§9§lGod Chestplate");
+					Item2Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item2Meta.addEnchant(Enchantment.DURABILITY, 2, true);
+					Item2.setItemMeta(Item2Meta);
+					
+					//God Leggings
+					ItemStack Item3 = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+					ItemMeta Item3Meta = Item3.getItemMeta();
+					Item3Meta.setDisplayName("§9§lGod Leggings");
+					Item3Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item3Meta.addEnchant(Enchantment.DURABILITY, 2, true);
+					Item3.setItemMeta(Item3Meta);
+					
+					//God Boots
+					ItemStack Item4 = new ItemStack(Material.DIAMOND_BOOTS, 1);
+					ItemMeta Item4Meta = Item4.getItemMeta();
+					Item4Meta.setDisplayName("§9§lGod Boots");
+					Item4Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item4Meta.addEnchant(Enchantment.DURABILITY, 2, true);
+					Item4.setItemMeta(Item4Meta);
+					
+					//God Sword
+					ItemStack Item5 = new ItemStack(Material.DIAMOND_SWORD, 1);
+					ItemMeta Item5Meta = Item5.getItemMeta();
+					Item5Meta.setDisplayName("§9§lGod Sword");
+					Item5Meta.addEnchant(Enchantment.DAMAGE_ALL, 5, true);
+					Item5Meta.addEnchant(Enchantment.FIRE_ASPECT, 1, true);
+					Item5.setItemMeta(Item5Meta);
+					
+					//God Bow
+					ItemStack Item6 = new ItemStack(Material.BOW, 1);
+					ItemMeta Item6Meta = Item6.getItemMeta();
+					Item6Meta.setDisplayName("§9§lGod Bow");
+					Item6Meta.addEnchant(Enchantment.ARROW_DAMAGE, 5, true);
+					Item6Meta.addEnchant(Enchantment.ARROW_FIRE, 1, true);
+					Item6.setItemMeta(Item6Meta);
+					
+					//Arrows
+					ItemStack Item7 = new ItemStack(Material.ARROW, 128);
+					
+					//Elixir of Fury
+					ItemStack Item8 = new ItemStack(Material.POTION, 1, (byte) 8266);
+					ItemMeta Item8Meta = Item6.getItemMeta();
+					List<String> lore8 = new ArrayList<String>();
+					Item8Meta.setDisplayName("§4§l§nElixir of Fury");
+					Item8Meta.addEnchant(Enchantment.DIG_SPEED, 5, true);
+					lore8.add("§eSpeed II (1:30)");
+					lore8.add("§eStrength II (1:30)");
+					lore8.add("§eFire Resistence (8:00)");
+					lore8.add("§1");
+					lore8.add("§cPrice: §f5 Coins");
+					Item8Meta.setLore(lore8);
+					Item8.setItemMeta(Item8Meta);
+					
+					player.getInventory().addItem(Item1);
+					player.getInventory().addItem(Item2);
+					player.getInventory().addItem(Item3);
+					player.getInventory().addItem(Item4);
+					player.getInventory().addItem(Item5);
+					player.getInventory().addItem(Item6);
+					player.getInventory().addItem(Item7);
+					player.getInventory().addItem(Item8);
+					player.getInventory().addItem(Item8);
+				}else if (args[0].equalsIgnoreCase("Icewynd")) {
+					//Icewynd Helmet
+					ItemStack Item1 = new ItemStack(Material.DIAMOND_HELMET, 1);
+					ItemMeta Item1Meta = Item1.getItemMeta();
+					Item1Meta.setDisplayName("§b§lIcewynd Helmet");
+					Item1Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item1Meta.addEnchant(Enchantment.DURABILITY, 3, true);
+					Item1.setItemMeta(Item1Meta);
+					
+					//Icewynd Chestplate
+					ItemStack Item2 = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+					ItemMeta Item2Meta = Item2.getItemMeta();
+					Item2Meta.setDisplayName("§b§lIcewynd Chestplate");
+					Item2Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item2Meta.addEnchant(Enchantment.DURABILITY, 3, true);
+					Item2.setItemMeta(Item2Meta);
+					
+					//Icewynd Leggings
+					ItemStack Item3 = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+					ItemMeta Item3Meta = Item3.getItemMeta();
+					Item3Meta.setDisplayName("§b§lIcewynd Leggings");
+					Item3Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item3Meta.addEnchant(Enchantment.DURABILITY, 3, true);
+					Item3.setItemMeta(Item3Meta);
+					
+					//Icewynd Boots
+					ItemStack Item4 = new ItemStack(Material.DIAMOND_BOOTS, 1);
+					ItemMeta Item4Meta = Item4.getItemMeta();
+					Item4Meta.setDisplayName("§b§lIcewynd Boots");
+					Item4Meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+					Item4Meta.addEnchant(Enchantment.DURABILITY, 3, true);
+					Item4.setItemMeta(Item4Meta);
+					
+					//Icewynd Sword
+					ItemStack Item5 = new ItemStack(Material.DIAMOND_SWORD, 1);
+					ItemMeta Item5Meta = Item5.getItemMeta();
+					Item5Meta.setDisplayName("§b§lIcewynd Sword");
+					Item5Meta.addEnchant(Enchantment.DAMAGE_ALL, 5, true);
+					Item5Meta.addEnchant(Enchantment.FIRE_ASPECT, 2, true);
+					Item5Meta.addEnchant(Enchantment.DURABILITY, 3, true);
+					Item5.setItemMeta(Item5Meta);
+					
+					//Icewynd Bow
+					ItemStack Item6 = new ItemStack(Material.BOW, 1);
+					ItemMeta Item6Meta = Item6.getItemMeta();
+					Item6Meta.setDisplayName("§b§lIcewynd Bow");
+					Item6Meta.addEnchant(Enchantment.ARROW_DAMAGE, 5, true);
+					Item6Meta.addEnchant(Enchantment.ARROW_FIRE, 2, true);
+					Item6Meta.addEnchant(Enchantment.DURABILITY, 3, true);
+					Item6Meta.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+					Item6.setItemMeta(Item6Meta);
+					
+					//Arrows
+					ItemStack Item7 = new ItemStack(Material.ARROW, 1);
+					
+					//Elixir of Fury
+					ItemStack Item8 = new ItemStack(Material.POTION, 1, (byte) 8266);
+					ItemMeta Item8Meta = Item6.getItemMeta();
+					List<String> lore8 = new ArrayList<String>();
+					Item8Meta.setDisplayName("§4§l§nElixir of Fury");
+					Item8Meta.addEnchant(Enchantment.DIG_SPEED, 5, true);
+					lore8.add("§eSpeed II (1:30)");
+					lore8.add("§eStrength II (1:30)");
+					lore8.add("§eFire Resistence (8:00)");
+					lore8.add("§1");
+					lore8.add("§cPrice: §f5 Coins");
+					Item8Meta.setLore(lore8);
+					Item8.setItemMeta(Item8Meta);
+					
+					player.getInventory().addItem(Item1);
+					player.getInventory().addItem(Item2);
+					player.getInventory().addItem(Item3);
+					player.getInventory().addItem(Item4);
+					player.getInventory().addItem(Item5);
+					player.getInventory().addItem(Item6);
+					player.getInventory().addItem(Item7);
+					player.getInventory().addItem(Item8);
+					player.getInventory().addItem(Item8);
+					player.getInventory().addItem(Item8);
+			}
 			}
 		}
 		return false;
@@ -139,15 +467,5 @@ Main plugin = Main.getPlugin(Main.class);
 		kit.setItem(4, Item5);//Icewynd
 		player.openInventory(kit);
 		}
-	
-
-	@SuppressWarnings("deprecation")
-	@EventHandler
-	public void onPlace(BlockPlaceEvent event) {
-		if (event.getBlock().getType().equals(Material.PISTON_BASE)) {
-			if (event.getPlayer().getItemInHand().getItemMeta().getDisplayName().contains("§n")) {
-				event.getPlayer().getWorld().playEffect(event.getBlock().getLocation(), Effect.STEP_SOUND, Material.GOLD_BLOCK);
-			}
-		}
-	}
 }
+	
