42eed4121a6d04504ae2b947c8f4fbdc3ad220d6
Mag <Mag@DESKTOP-GCOO917>
2018-02-04 01:01:23 -0500

    Disable freecam on logout

--- IcewyndCore/src/Commands/Freecam.java
+++ IcewyndCore/src/Commands/Freecam.java
@@ -5,25 +5,37 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import Main.Main;
 
-public class Freecam implements CommandExecutor{
+public class Freecam implements CommandExecutor, Listener{
 Main plugin = Main.getPlugin(Main.class);
-
-public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	if (cmd.getName().equalsIgnoreCase("freecam")) {
-		Player player = (Player) sender;
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("freecam")) {
+			Player player = (Player) sender;
+			if (plugin.getConfig().getBoolean(player.getUniqueId() + ".Freecam") == false) {
+				plugin.getConfig().set(player.getUniqueId() + ".Freecam", true);
+				player.sendMessage("§e§l(!)§7 Freecam has been enabled!");
+				player.setGameMode(GameMode.SPECTATOR);
+			}else {
+				plugin.getConfig().set(player.getUniqueId() + ".Freecam", false);
+				player.sendMessage("§e§l(!)§7 Freecam has been disabled!");
+				player.setGameMode(GameMode.SURVIVAL);
+			}
+		}
+		return false;
+	}
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
 		if (plugin.getConfig().getBoolean(player.getUniqueId() + ".Freecam") == true) {
-			plugin.getConfig().set(player.getUniqueId() + ".Freecam", false);
-			player.sendMessage("§e§l(!)§7 Freecam has been disabled!");
+			player.teleport(player.getWorld().getSpawnLocation());
 			player.setGameMode(GameMode.SURVIVAL);
-		}else {
-			plugin.getConfig().set(player.getUniqueId() + ".Freecam", true);
-			player.sendMessage("§e§l(!)§7 Freecam has been enabled!");
-			player.setGameMode(GameMode.SPECTATOR);
+			plugin.getConfig().set(player.getUniqueId() + ".Freecam", false);
 		}
 	}
-	return false;
-}
 }
