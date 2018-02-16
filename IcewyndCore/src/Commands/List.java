7f9e9df5083a5cc2efc9388450a740cb70ce7c11
Mag <Mag@DESKTOP-BBMJSM3>
2018-02-16 17:43:03 -0500

    Ignore

---
+++ IcewyndCore/src/Commands/List.java
@@ -0,0 +1,21 @@
+package Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import Main.Main;
+
+public class List implements CommandExecutor{
+Main plugin = Main.getPlugin(Main.class);
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("list")) {
+			if (sender instanceof Player) {
+				sender.sendMessage("Admins:");
+			}
+		}
+		return false;
+	}
+}
