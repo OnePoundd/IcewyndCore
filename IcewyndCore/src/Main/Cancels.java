4c4e18434924828e16b41d8efd5f96ba367c65a0
Mag <Mag@DESKTOP-BBMJSM3>
2018-02-15 21:52:42 -0500

    Disabled endermite spawning

--- IcewyndCore/src/Main/Cancels.java
+++ IcewyndCore/src/Main/Cancels.java
@@ -1,13 +1,19 @@
 package Main;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class Cancels implements Listener {
 
 	@EventHandler
 	public void onBreed(EntityBreedEvent e) {
+		e.setCancelled(true);
+	}
+	public void onspawn(EntitySpawnEvent e) {
+		if (e.getEntityType() == EntityType.ENDERMITE);
 		e.setCancelled(true);
 	}
 }
