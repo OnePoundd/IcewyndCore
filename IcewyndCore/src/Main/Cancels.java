381eb90db8e45f62df71b8861a35ae633cd384f1
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:53:07 +0000

    Update Cancels.java

--- IcewyndCore/src/Main/Cancels.java
+++ IcewyndCore/src/Main/Cancels.java
@@ -1,10 +1,13 @@
 package Main;
 
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityBreedEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
 
 public class Cancels implements Listener {
 
@@ -18,9 +21,21 @@ public class Cancels implements Listener {
 			e.setCancelled(true);
 		}
 	}
-	//@EventHandler
-	//public void onMobSpawn(CreatureSpawnEvent event) {
-	//LivingEntity entity = event.getEntity();
-	//entity.setAI(false);
-	//}
+	public void noAI(org.bukkit.entity.Entity bukkitEntity)
+	  {
+	    net.minecraft.server.v1_8_R3.Entity nmsEntity = ((CraftEntity)bukkitEntity).getHandle();
+	    NBTTagCompound tag = nmsEntity.getNBTTag();
+	    if (tag == null) {
+	      tag = new NBTTagCompound();
+	    }
+	    nmsEntity.c(tag);
+	    tag.setInt("NoAI", 1);
+	    nmsEntity.f(tag);
+	  }
+	
+	@EventHandler
+	public void onMobSpawn(CreatureSpawnEvent event) {
+		LivingEntity entity = event.getEntity();
+		noAI(entity);
+	}
 }
