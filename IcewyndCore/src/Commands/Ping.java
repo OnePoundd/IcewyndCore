f34280ccdf58430fdcf5054780dbc4a67d130b23
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:47:40 +0000

    Cleanup

--- IcewyndCore/src/Commands/Ping.java
+++ IcewyndCore/src/Commands/Ping.java
@@ -4,12 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-import Main.Main;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 
 public class Ping implements CommandExecutor{
-	Main plugin = Main.getPlugin(Main.class);
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("ping")) {
@@ -17,19 +15,19 @@ public class Ping implements CommandExecutor{
 			if (sender instanceof Player) {
 				if (args.length == 0) {
 					int ping = ((CraftPlayer) player).getHandle().ping;
-					player.sendMessage("§aPing §7»§f " + ping);
+					player.sendMessage("Â§aPing Â§7Â»Â§f " + ping);
 				}
 			} else if (args.length == 1) {
 				Player target = Bukkit.getPlayer(args[0]);
 				if(!(target == null)) {
 					if (target.isOnline()) {
 						int targetPing = ((CraftPlayer) target).getHandle().ping;
-						player.sendMessage("§a" + target.getName() + "'s §aPing §7»§f " + targetPing);
+						player.sendMessage("Â§a" + target.getName() + "'s Â§aPing Â§7Â»Â§f " + targetPing);
 					}else if (!target.isOnline()) {
-						player.sendMessage("§cThat player is not online!");
+						player.sendMessage("Â§cThat player is not online!");
 					}
 				}else {
-					player.sendMessage("§cThat player cannot be found or is offline.");
+					player.sendMessage("Â§cThat player cannot be found or is offline.");
 				}
 			}
 		}
