a8f3a3faa55a028e86a405b300ad0f30abd44392
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:36:17 +0000

    Cleanup

--- IcewyndCore/src/Commands/Enderchest.java
+++ IcewyndCore/src/Commands/Enderchest.java
@@ -4,16 +4,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import Main.Main;
 
 public class Enderchest implements CommandExecutor{
-	Main plugin = Main.getPlugin(Main.class);
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("enderchest")) {
 			Player player = (Player) sender;
 			player.openInventory(player.getEnderChest());
-			player.sendMessage("ง7Opening Enderchest...");
+			player.sendMessage("ยง7Opening Enderchest...");
 		}
 		return false;
 	}
