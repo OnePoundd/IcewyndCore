7f142fcdd95676d376bca716e873949d121fe95f
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:33:18 +0000

    1.8 Conversions and cleanup

--- IcewyndCore/src/BanSystem/BanCommand.java
+++ IcewyndCore/src/BanSystem/BanCommand.java
@@ -5,9 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
 import com.massivecraft.factions.entity.MConf;
-
 import Main.Main;
 
 public class BanCommand implements CommandExecutor {
@@ -21,9 +19,9 @@ public class BanCommand implements CommandExecutor {
 					Player BannedPlayer = Bukkit.getPlayer(args[0]);
 					if (cmd.getName().equalsIgnoreCase("ban")) {
 						if (args.length == 3) {
-							Bukkit.broadcastMessage("§c§l[Ban] §7" + args[0] + " has been banned for " + args[1] );
+							Bukkit.broadcastMessage("Â§cÂ§l[Ban] Â§7" + args[0] + " has been banned for " + args[1] );
 							plugin.getConfig().set(BannedPlayer.getUniqueId() + ".Banned", true);
-							BannedPlayer.sendMessage("§c§lATTENTION! §eYou've been banned for " + args[2] + ". §eBans are handled differently here. Please read the holograms for more information.");
+							BannedPlayer.sendMessage("Â§cÂ§lATTENTION! Â§eYou've been banned for " + args[2] + ". Â§eBans are handled differently here. Please read the holograms for more information.");
 							BannedPlayer.getInventory().clear();
 							BannedPlayer.setLevel(0);
 							if(MConf.get().getWarp("jail") != null) {
@@ -35,10 +33,10 @@ public class BanCommand implements CommandExecutor {
 
 					}
 				}else {
-					sender.sendMessage("§c§l(!)§7 The player cannot be found!");
+					sender.sendMessage("Â§cÂ§l(!)Â§7 The player cannot be found!");
 				}
 			}else {
-				sender.sendMessage("§c§l(!)§7 Usage: /ban <player> <reason> <duration>");
+				sender.sendMessage("Â§cÂ§l(!)Â§7 Usage: /ban <player> <reason> <duration>");
 			}
 		}
 		return false;
