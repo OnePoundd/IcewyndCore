da2e77e0c9d4ac1537963b340a0ea5f86cb66c36
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:47:12 +0000

    Cleanup

--- IcewyndCore/src/Commands/PInfo.java
+++ IcewyndCore/src/Commands/PInfo.java
@@ -4,13 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import com.massivecraft.factions.entity.MPlayer;
-import Main.Main;
 
 public class PInfo implements CommandExecutor{
-	Main plugin = Main.getPlugin(Main.class);
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("pinfo")) {
@@ -21,16 +19,16 @@ public class PInfo implements CommandExecutor{
 				String faction = mplayer.getFactionName();
 				Player target = Bukkit.getPlayer(args[0]);
 				int targetPing = ((CraftPlayer) target).getHandle().ping;
-				p.sendMessage("§8§l§m-------§r§8§l[ §a§l" + args[0] + " §r§8§l]§8§l§m-------");
-				p.sendMessage("§cIP:§f ");
+				p.sendMessage("Â§8Â§lÂ§m-------Â§rÂ§8Â§l[ Â§aÂ§l" + args[0] + " Â§rÂ§8Â§l]Â§8Â§lÂ§m-------");
+				p.sendMessage("Â§cIP:Â§f ");
 				//target.getAddress().getHostName()); (DISABLED FOR NOW, dont remove)
-				p.sendMessage("§cPing:§f " + targetPing);
-				p.sendMessage("§cFirst Seen:§f " + target.getFirstPlayed());
-				p.sendMessage("§cLast Seen:§f " + target.getLastPlayed());
-				p.sendMessage("§cFaction:§f " + faction);
-				p.sendMessage("§cXP Level:§f " + target.getExpToLevel());
-				p.sendMessage("§cServer Level:§f " + MPlayer.get(target).getLevel());
-				p.sendMessage("§cGamemode:§f " + target.getGameMode());
+				p.sendMessage("Â§cPing:Â§f " + targetPing);
+				p.sendMessage("Â§cFirst Seen:Â§f " + target.getFirstPlayed());
+				p.sendMessage("Â§cLast Seen:Â§f " + target.getLastPlayed());
+				p.sendMessage("Â§cFaction:Â§f " + faction);
+				p.sendMessage("Â§cXP Level:Â§f " + target.getExpToLevel());
+				p.sendMessage("Â§cServer Level:Â§f " + MPlayer.get(target).getLevel());
+				p.sendMessage("Â§cGamemode:Â§f " + target.getGameMode());
 			}
 		}
 		return false;
