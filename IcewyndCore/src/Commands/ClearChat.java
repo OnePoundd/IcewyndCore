b9fb18cc3c5ce32727c2dc533eb6bc73fe081b4e
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:34:45 +0000

    1.8 Conversions and cleanup

--- IcewyndCore/src/Commands/ClearChat.java
+++ IcewyndCore/src/Commands/ClearChat.java
@@ -4,18 +4,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import Main.Main;
 
 public class ClearChat implements CommandExecutor{
-	Main plugin = Main.getPlugin(Main.class);
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("clearchat")) {
 			if (sender.hasPermission("server.admin")) {
 				for (int i = 0; i < 150; i++) {
-					Bukkit.broadcastMessage("§1");
+					Bukkit.broadcastMessage("Â§1");
 				}
-				Bukkit.broadcastMessage("§f§l[§b§lICEWYND§f§l]§a§l Chat has been cleared!");
+				Bukkit.broadcastMessage("Â§fÂ§l[Â§bÂ§lICEWYNDÂ§fÂ§l]Â§aÂ§l Chat has been cleared!");
 			}
 		}
 		return false;
