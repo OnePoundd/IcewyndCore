9e31a2b1a2d489aeed25ecdf103b798e4ec33eae
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:35:35 +0000

    Cleannup

--- IcewyndCore/src/Commands/Disposal.java
+++ IcewyndCore/src/Commands/Disposal.java
@@ -6,16 +6,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import Main.Main;
 
 public class Disposal implements CommandExecutor{
-	Main plugin = Main.getPlugin(Main.class);
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("disposal")) {
 			if (sender instanceof Player) {
 				Player player = (Player) sender;
-				Inventory Disposal = Bukkit.createInventory(null, 27, "Drop items here to be deleted.");
+				Inventory Disposal = Bukkit.createInventory(null, 27, "§cDeposit items here to be deleted..");
 				player.openInventory(Disposal);
 			}
 		}
