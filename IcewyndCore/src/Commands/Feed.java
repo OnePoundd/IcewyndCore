18493d8b25be399dcccf717b070a50fecdbdb817
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:36:30 +0000

    Cleanup

--- IcewyndCore/src/Commands/Feed.java
+++ IcewyndCore/src/Commands/Feed.java
@@ -5,13 +5,17 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Enderchest implements CommandExecutor{
+public class Feed implements CommandExecutor{
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("enderchest")) {
-			Player player = (Player) sender;
-			player.openInventory(player.getEnderChest());
-			player.sendMessage("§7Opening Enderchest...");
+		if (cmd.getName().equalsIgnoreCase("feed")) {
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				if (player.hasPermission("server.feed")) {
+					player.setFoodLevel(20);
+					player.sendMessage("§7Your hunger has been replenished.");
+				}
+			}
 		}
 		return false;
 	}
