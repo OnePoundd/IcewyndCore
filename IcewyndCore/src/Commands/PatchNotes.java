436103989a7469afcf9788f358cf3f5055d4739f
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:47:26 +0000

    Cleanup

--- IcewyndCore/src/Commands/PatchNotes.java
+++ IcewyndCore/src/Commands/PatchNotes.java
@@ -4,21 +4,19 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import Main.Main;
 
 public class PatchNotes implements CommandExecutor{
-	Main plugin = Main.getPlugin(Main.class);
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("patchnotes")) {
 			if (sender instanceof Player) {
 				Player player = (Player) sender;
-				player.sendMessage("§8§l§m--------§8§l[§e§l Patch Notes §8§l]§8§l§m--------");
-				player.sendMessage("§7>> §c§lDecember 1st - December 7th:");
-				player.sendMessage("§a[+]§f Text1");
-				player.sendMessage("§a[+]§f Text1");
-				player.sendMessage("§c[-]§f Text1");
-				player.sendMessage("§c[-]§f Text1");
+				player.sendMessage("Â§8Â§lÂ§m--------Â§8Â§l[Â§eÂ§l Patch Notes Â§8Â§l]Â§8Â§lÂ§m--------");
+				player.sendMessage("Â§7>> Â§cÂ§lDecember 1st - December 7th:");
+				player.sendMessage("Â§a[+]Â§f Text1");
+				player.sendMessage("Â§a[+]Â§f Text1");
+				player.sendMessage("Â§c[-]Â§f Text1");
+				player.sendMessage("Â§c[-]Â§f Text1");
 			}
 		}
 		return false;
