53f04ad11cbc82ee3785d7d3a564db3e7db52208
Mag <35281701+Magnettar@users.noreply.github.com>
2018-03-03 13:35:08 +0000

    Cleanup

--- IcewyndCore/src/Commands/ClearInventory.java
+++ IcewyndCore/src/Commands/ClearInventory.java
@@ -4,21 +4,17 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import Main.Main;
 
 public class ClearInventory implements CommandExecutor{
-	Main plugin = Main.getPlugin(Main.class);
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("ci")) {
 			if (sender instanceof Player) {
 				Player player = (Player) sender;
-				ItemStack[] armorContents = player.getInventory().getArmorContents().clone();
 				player.getInventory().clear();
-				player.getInventory().setArmorContents(armorContents);
+				player.getInventory().setArmorContents(player.getInventory().getArmorContents().clone());
 				player.updateInventory();
-				player.sendMessage("§eYour inventory has been cleared");
+				player.sendMessage("Â§eYour inventory has been cleared");
 			}
 		}
 		return false;
