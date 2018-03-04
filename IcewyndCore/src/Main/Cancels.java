60f1daac737c256a40a11e6ac54fbf613113d584
Mag <Mag@DESKTOP-BBMJSM3>
2018-03-03 21:49:51 -0500

    Progress

--- IcewyndCore/src/Main/Cancels.java
+++ IcewyndCore/src/Main/Cancels.java
@@ -1,12 +1,17 @@
 package Main;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
 import net.minecraft.server.v1_8_R3.NBTTagCompound;
 
 public class Cancels implements Listener {
@@ -42,4 +47,15 @@ public class Cancels implements Listener {
 			noAI(entity);
 		}
 	}
+	@EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e)
+    {
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK && e.getAction() != Action.LEFT_CLICK_BLOCK)
+            return;
+       
+        Block b = e.getClickedBlock();
+       
+        if (b.getType() != Material.MOB_SPAWNER)
+            return;
+    }
 }
